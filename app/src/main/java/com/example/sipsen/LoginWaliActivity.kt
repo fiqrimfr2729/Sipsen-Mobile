a838d97d6d916a31ae46e78272bd1314f8e3190f
fiqrimfr2729 <madridistafiqri@gmail.com>
2019-10-30 15:10:34 +0700

    first commit

---
+++ app/src/main/java/com/example/sipsen/LoginWaliActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sipsen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginWaliActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_wali)
+    }
+}
